9f6e10c48e4a3cd66c56525796d45e7ae7d0705b
Kaplya <kaplyak@protonmail.com>
2021-10-12 10:04:17 +0200

    Make navbar

--- productivity/views.py
+++ productivity/views.py
@@ -4,4 +4,19 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hellow, world!")
+    return render(request, "productivity/index.html")
+
+def register(request):
+    return Hello
+
+def login(request):
+    return Hello
+
+def logout(request):
+    return Hello
+
+def profile(request):
+    return Hello
+
+def timer(request):
+    return Hello
