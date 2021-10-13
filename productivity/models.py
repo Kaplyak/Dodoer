3eb459575bc3475ac996e902ff83e51ad66db3aa
Kaplya <kaplyak@protonmail.com>
2021-10-13 21:42:34 +0200

    Add Task Model.

--- productivity/models.py
+++ productivity/models.py
@@ -5,3 +5,15 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class Task(models.Model):
+    # Fields: user, title, description, timestamp, color?
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=2048)
+    description = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    color = models.CharField(max_length=128)
+
+#class Profile(models.Model):
+    # Fields: focus_time, ???
